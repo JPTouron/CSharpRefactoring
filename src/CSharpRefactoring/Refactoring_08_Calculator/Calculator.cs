610790ad55a7ac28357c48ec30468ead170d6cae
JP <jp.touron@gmail.com>
2019-07-20 19:38:50 -0300

    agregado de comentario pertinente para el metodo calculate del proyecto 08

--- src/CSharpRefactoring/Refactoring_08_Calculator/Calculator.cs
+++ src/CSharpRefactoring/Refactoring_08_Calculator/Calculator.cs
@@ -6,10 +6,15 @@ namespace Refactoring_08_Calculator
     {
         /// <summary>
         /// que hicimos:
-        /// creamos metodos de extension, para decimales (para usarlos para el parametro price en este caso)
-        /// de esa forma podemos tener metodos como ApplyDiscountForAccountStatus que sean aplicables a price
-        /// y asi organizar la forma en que los descuentos se aplican y aislar a esta clase de la responsabilidad de saber
-        /// cómo se calculan los descuentos
+        /// Utilizando el patron de diseño strategy: https://www.dofactory.com/net/strategy-design-pattern
+        /// pudimos reemplazar cada caso del switch por una clase especializada en calcular el descuento de una forma concreta
+        /// de esta forma ganamos:
+        ///     simplicidad en este metodo y una unica responsabilidad: invocar el calculador apropiado
+        ///     mantener el funcionamiento original intacto (todos los UTs pasan ok)
+        ///     que cada responsabilidad tenga su clase, garantizando clases que solo tienen una sola responsabilidad
+        ///     hacer mas agnóstico este metodo: este metodo no sabe como se calcula cada descuento
+        ///     hacer mas simple el mantenimiento del metodo y los calculadores de descuento: no hay mas switches ni metodos raros de extension
+        ///         si quiero modificar / agregar un descuento, modifico / agrego una clase mas en CalculationStrategies
         /// </summary>
         public decimal Calculate(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
         {
