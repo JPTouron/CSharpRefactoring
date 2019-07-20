09271502afb1f19abbcd6908e975d727fbc40a90
JP <jp.touron@gmail.com>
2019-07-20 18:02:42 -0300

    Initial commit
    Agregado del proyecto 0, que muestra los problemas (code smells) en una app de ejemplo
    Se agregan los UTs correspondientes para iniciar el trabajo

---
+++ src/CSharpRefactoring/UnitTests/ManualCalculations.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTests
+{
+    public static class ManualCalculations
+    {
+        /// <summary>
+        /// hacemos el calculo "a mano" del descuento que aplica la clase Calculator y que almacena en su variable interna 'disc'
+        /// </summary>
+        public static decimal CalculateDiscountBasedOnYears(int years)
+        {
+            return (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
+        }
+
+        /// <summary>
+        /// hacemos el calculo "a mano" que hace la calculadora cuando le pasan Type=2 por parametro
+        /// </summary>
+        public static decimal CalculateFinalResultWithType2(decimal amount, decimal disc)
+        {
+            return (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+        }
+
+        /// <summary>
+        /// hacemos el calculo "a mano" que hace la calculadora cuando le pasan Type=3 por parametro
+        /// </summary>
+        public static decimal CalculateFinalResultWithType3(decimal amount, decimal disc)
+        {
+            return (0.7m * amount) - disc * (0.7m * amount);
+        }
+
+        /// <summary>
+        /// hacemos el calculo "a mano" que hace la calculadora cuando le pasan Type=4 por parametro
+        /// </summary>
+        public static decimal CalculateFinalResultWithType4(decimal amount, decimal disc)
+        {
+            return (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+        }
+    }
+}
---
+++ src/CSharpRefactoring/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc2ec6e1-5f71-417c-99dd-ed6f66f29adc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
